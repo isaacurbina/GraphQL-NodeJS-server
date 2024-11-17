6aa27b9484fd28d9f8078405f3bcb5770c5cd9e0
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-17 11:07:18 -0600

    17. Installing GraphQL and Express (#1)

--- server/app.js
+++ server/app.js
@@ -1,7 +1,17 @@
+//#region constants
 const express = require('express')
-
 const app = express()
+const {graphqlHTTP} = require('express-graphql')
+//const schema = require('./schema');
+//#endregion
 
-app.listen(4000, () => {
+//#region setup
+app.use('/graphql', graphqlHTTP({
+    //schema: schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => { // localhost:4000
     console.log('Listening for requests on my awesome port 4000')
 })
+//#endregion
