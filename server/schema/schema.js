d6eaa6c237b6fe75e387048ba66bb7f60ed34e3a
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-19 14:31:29 -0600

    21. Adding a Hobby Type and RootQuery (#5)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,11 +2,19 @@ const graphql = require('graphql')
 var _ = require('lodash')
 
 var usersData = [
-    {id: '1', name: 'Bond', age: 36},
-    {id: '13', name: 'Anna', age: 26},
-    {id: '211', name: 'Bella', age: 16},
-    {id: '19', name: 'Gina', age: 26},
-    {id: '158', name: 'Georgina', age: 36}
+    {id: '1', name: 'Bond', age: 36, profession: "Programmer"},
+    {id: '13', name: 'Anna', age: 26, profession: "Baker"},
+    {id: '211', name: 'Bella', age: 16, profession: "Mechanic"},
+    {id: '19', name: 'Gina', age: 26, profession: "Painter"},
+    {id: '158', name: 'Georgina', age: 36, profession: "Teacher"}
+]
+
+var hobbiesData = [
+    {id: '1', title: 'Programming', description: 'Using computers to make the world a better place.'},
+    {id: '2', title: 'Rowing', description: 'Sweat and feel better before eating donuts.'},
+    {id: '3', title: 'Swimming', description: 'Get in the water and learn to become the water.'},
+    {id: '4', title: 'Fencing', description: 'A hobby for fency people.'},
+    {id: '5', title: 'Hiking', description: 'Wear hiking boots and explore the world.'},
 ]
 
 const {
@@ -22,9 +30,20 @@ const UserType = new graphql.GraphQLObjectType({
     name: 'User',
     description: 'Documentation for user...',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        profession: {type: GraphQLString}
+    })
+})
+
+const HobbyType = new graphql.GraphQLObjectType({
+    name: "Hobby",
+    description: "Hobby description",
+    fields: () => ({
+        id: {type: GraphQLID},
+        title: {type: GraphQLString},
+        description: {type: GraphQLString}
     })
 })
 
@@ -35,10 +54,18 @@ const RootQuery = new GraphQLObjectType({
     fields: () => ({
         user: {
             type: UserType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
 
             resolve(parent, args) {
                 return _.find(usersData, { id: args.id})
+            }
+        },
+        hobby: {
+            type: HobbyType,
+            arg: {id: {type: GraphQLID}},
+
+            resolve(parent, args) {
+                return _.find(hobbiesData, {id: args.id})
             }
         }
     })
