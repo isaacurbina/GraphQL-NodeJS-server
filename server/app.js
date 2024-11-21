82b37f903a80b313272e1a4687605110a09a5823
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-21 16:34:07 -0600

    39. Installing Mongoose Package and Connecting

--- server/app.js
+++ server/app.js
@@ -4,14 +4,24 @@ const app = express()
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema');
 const testSchema = require('./schema/types_schema')
+const mongoose = require('mongoose')
+const mongooseUrl = 'mongodb+srv://iurbinacantu:KjLWhDoSlK1WPlJK@gq-course.7ool4.mongodb.net/'
 //#endregion
 
 //#region setup
+// Mongoose
+mongoose.connect(mongooseUrl)
+mongoose.connection.once('open', () => {
+    console.log('Yes! We are connected!')
+})
+
+// GraphQL
 app.use('/graphql', graphqlHTTP({
     schema: testSchema,
     graphiql: true
 }))
 
+// App
 app.listen(4000, () => { // localhost:4000
     console.log('Listening for requests on my awesome port 4000')
 })
