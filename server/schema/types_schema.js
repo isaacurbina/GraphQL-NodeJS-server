29ae843f31db1befe7df0e65d1890c5f9a179837
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-21 15:56:04 -0600

    36. GraphQL - Scalar Types and Object Types

--- server/schema/types_schema.js
+++ server/schema/types_schema.js
@@ -20,7 +20,14 @@ const Person = new GraphQLObjectType({
         name: {type: new GraphQLNonNull(GraphQLString)},
         age: {type: new GraphQLNonNull(GraphQLInt)},
         isMarried: {type: GraphQLBoolean},
-        gpa: {type: GraphQLFloat}
+        gpa: {type: GraphQLFloat},
+
+        justAType: {
+            type: Person,
+            resolve(parent, args) {
+                return parent
+            }
+        }
     })
 })
 
@@ -29,7 +36,18 @@ const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     description: "Description",
     fields: {
-
+        person: {
+            type: Person,
+            resolve(parent, args) {
+                let personObj = {
+                    name: 'Antonio',
+                    age: 35,
+                    isMarried: true,
+                    gpa: 4.0
+                }
+                return personObj
+            }
+        }
     }
 });
 
