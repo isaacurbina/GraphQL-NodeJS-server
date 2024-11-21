e5003f2cac12ac1af9e385bbc2212e23c4e56a5f
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-21 15:56:35 -0600

    36. GraphQL - Scalar Types and Object Types (#16)

--- server/app.js
+++ server/app.js
@@ -3,11 +3,12 @@ const express = require('express')
 const app = express()
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema');
+const testSchema = require('./schema/types_schema')
 //#endregion
 
 //#region setup
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
+    schema: testSchema,
     graphiql: true
 }))
 
