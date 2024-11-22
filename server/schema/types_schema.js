6d1b2330ae5aa4efc710adbd2f3ab3cc81d4babe
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-22 15:45:19 -0600

    41. Mutation in GraphQL - Updating a User Type (#19)

--- server/schema/types_schema.js
+++ server/schema/types_schema.js
@@ -1,56 +1,56 @@
-const graphql = require('graphql');
+const graphql = require("graphql");
 
 const {
-    GraphQLObjectType,
-    GraphQLID,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLBoolean,
-    GraphQLFloat,
-    GraphQLSchema,
-    GraphQLNonNull
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLSchema,
+  GraphQLNonNull,
 } = graphql;
 
 // Scalar Type
 const Person = new GraphQLObjectType({
-    name: 'Person',
-    description: 'Represents a Person Type',
-    fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: new GraphQLNonNull(GraphQLString)},
-        age: {type: new GraphQLNonNull(GraphQLInt)},
-        isMarried: {type: GraphQLBoolean},
-        gpa: {type: GraphQLFloat},
+  name: "Person",
+  description: "Represents a Person Type",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    age: { type: new GraphQLNonNull(GraphQLInt) },
+    isMarried: { type: GraphQLBoolean },
+    gpa: { type: GraphQLFloat },
 
-        justAType: {
-            type: Person,
-            resolve(parent, args) {
-                return parent
-            }
-        }
-    })
-})
+    justAType: {
+      type: Person,
+      resolve(parent, args) {
+        return parent;
+      },
+    },
+  }),
+});
 
 // RootQuery
 const RootQuery = new GraphQLObjectType({
-    name: "RootQueryType",
-    description: "Description",
-    fields: {
-        person: {
-            type: Person,
-            resolve(parent, args) {
-                let personObj = {
-                    name: 'Antonio',
-                    age: 35,
-                    isMarried: true,
-                    gpa: 4.0
-                }
-                return personObj
-            }
-        }
-    }
+  name: "RootQueryType",
+  description: "Description",
+  fields: {
+    person: {
+      type: Person,
+      resolve(parent, args) {
+        let personObj = {
+          name: "Antonio",
+          age: 35,
+          isMarried: true,
+          gpa: 4.0,
+        };
+        return personObj;
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery,
 });
